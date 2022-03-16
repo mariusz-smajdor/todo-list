db90296b48505d25ff65eece9b944c72ef670454
Mariusz <smajdormariusz98@gmail.com>
2022-03-16 18:16:05 +0100

    prevented form from submission

--- js/script.js
+++ js/script.js
@@ -27,5 +27,17 @@
     tasksElement.innerHTML = htmlString
   }
 
-  render()
+  const onFormSubmit = event => {
+    event.preventDefault()
+  }
+
+  const init = () => {
+    render()
+
+    const formElement = document.querySelector(".js-form")
+
+    formElement.addEventListener("submit", onFormSubmit)
+  }
+
+  init()
 }
